21183f92d57c3a4bfe155fab6a95264c7e0e467c
garthendrich <garthendrich@gmail.com>
2022-08-18 23:10:49 +0800

    Add custom themes to Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#f1f5fc",
+          100: "#e5eefa",
+          200: "#cfdff6",
+          300: "#b2c9ef",
+          400: "#93abe6",
+          500: "#788fdc",
+          600: "#5e6fcd",
+          700: "#4e5cb4",
+          800: "#414d92",
+          900: "#232946",
+        },
+        stroke: "#121629",
+      },
+      boxShadow: {
+        button: "0 4px #64748B",
+      },
+    },
   },
   plugins: [],
 };
