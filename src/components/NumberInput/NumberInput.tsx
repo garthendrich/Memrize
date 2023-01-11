ec02bc3450b5445cae6b88d42b39f5f7c26a27ee
garthendrich <garthendrich@gmail.com>
2023-01-11 21:25:25 +0800

    Limit number input on keydown

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -1,22 +1,34 @@
+const limitToRange = (number: number, min: number, max: number) =>
+  Math.max(min, Math.min(max, number));
+
 export interface NumberInputProps {
+  className?: string;
   value: number;
   setValue(value: number): void;
   min?: number;
   max?: number;
 }
 
-export function NumberInput({ value, setValue, min, max }: NumberInputProps) {
+export function NumberInput({
+  className = "",
+  value,
+  setValue,
+  min = -Infinity,
+  max = Infinity,
+}: NumberInputProps) {
   return (
     <input
-      className="rounded border border-slate-400 bg-slate-100 px-3 py-2 text-stroke placeholder:text-slate-400 focus:border-primary-900"
+      className={`rounded border border-slate-400 bg-slate-100 px-3 py-2 text-stroke placeholder:text-slate-400 focus:border-primary-900 ${className}`}
       type="number"
       value={String(value)}
-      onKeyPress={(event) => {
-        if (!/[0-9]/.test(event.key)) {
+      onKeyDown={(event) => {
+        if (".e".includes(event.key)) {
           event.preventDefault();
         }
       }}
-      onChange={(event) => setValue(Number(event.target.value))}
+      onChange={(event) =>
+        setValue(limitToRange(Number(event.target.value), min, max))
+      }
       min={min}
       max={max}
     />
@@ -24,6 +36,7 @@ export function NumberInput({ value, setValue, min, max }: NumberInputProps) {
 }
 
 NumberInput.defaultProps = {
-  min: Number.MIN_SAFE_INTEGER,
-  max: Number.MAX_SAFE_INTEGER,
+  className: "",
+  min: -Infinity,
+  max: Infinity,
 };
