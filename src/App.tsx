fc9687832de80c9b6645da1bc46335a5de59ef79
garthendrich <garthendrich@gmail.com>
2023-01-11 09:55:15 +0800

    Refactor background color

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { HomePage } from "@/pages";
 
 function App() {
-  return (
-    <div className="min-h-screen bg-primary-900">
-      <HomePage />
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default App;
