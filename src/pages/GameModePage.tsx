5a3fd1a972f0c9a9dbd9d3078f13404e104e36fd
garthendrich <garthendrich@gmail.com>
2023-01-15 13:24:58 +0800

    Remove unnecessary back button

--- src/pages/GameModePage.tsx
+++ src/pages/GameModePage.tsx
@@ -2,8 +2,6 @@ import { MemrizeLogo } from "@/assets/svgs";
 import { Button, Header, MainPanel } from "@/components";
 import { GameMode, gameModeInfo } from "@/shared";
 import { capitalize } from "@/utils/capitalize";
-import { ArrowLeftIcon } from "@heroicons/react/24/solid";
-import { Link } from "react-router-dom";
 
 export interface GameModePageProps {
   gameMode: GameMode;
@@ -13,18 +11,10 @@ export function GameModePage({ gameMode }: GameModePageProps) {
   return (
     <>
       <Header isFixed>
-        <Header.Segment justify="justify-start">
-          <Link to="/">
-            <Button variant="outlined">
-              <ArrowLeftIcon className="h-6 w-6 fill-white" />
-            </Button>
-          </Link>
-        </Header.Segment>
         <Header.Segment justify="justify-center">
           <MemrizeLogo className="mr-4 h-8 w-8" />
           <p className="text-2xl text-white">Memrize</p>
         </Header.Segment>
-        <Header.Segment />
       </Header>
       <MainPanel>
         <div className="flex min-h-screen flex-col items-center justify-center gap-8 bg-stroke px-12">
