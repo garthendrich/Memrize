71e57c475202e1825d43b786199688808d7250ad
garthendrich <garthendrich@gmail.com>
2023-02-05 15:31:04 +0800

    Fix item flasher

--- src/components/ItemFlasher/ItemFlasher.tsx
+++ src/components/ItemFlasher/ItemFlasher.tsx
@@ -14,7 +14,7 @@ export function ItemFlasher({ items }: ItemFlasherProps) {
     seconds: timerDuration,
   });
 
-  const itemIndex = Math.floor((timerDuration - timer) / 2);
+  const itemIndex = Math.floor((timerDuration - timer) / flashDuration);
 
   return (
     <MainPanel>
