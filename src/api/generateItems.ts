852bbe2477c8f0351e7725c2d35899adeef2e582
garthendrich <garthendrich@gmail.com>
2023-02-06 22:44:54 +0800

    Prevent consecutive duplicate items
    In flash game modes, consecutive duplicate items might not be noticed

--- src/api/generateItems.ts
+++ src/api/generateItems.ts
@@ -2,23 +2,37 @@ import randomWords from "random-words";
 
 import { GameMode } from "@/shared";
 
+const itemLength = 100;
+
 const randomNumberStrings = () => {
-  const numbers = Array.from({ length: 100 }, () =>
+  const numbers = Array.from({ length: itemLength }, () =>
     Math.floor(Math.random() * 100)
   );
 
   return numbers.map((number) => number.toString().padStart(2, "0"));
 };
 
+const hasConsecutiveDuplicates = (items: string[]) =>
+  items.some((item, itemIndex) => item === items[itemIndex + 1]);
+
 export const generateItems = (gameMode: GameMode) => {
-  switch (gameMode) {
-    case "classic words":
-    case "flash words":
-      return randomWords({ exactly: 100 });
-    case "classic numbers":
-    case "flash numbers":
-      return randomNumberStrings();
-    default:
-      return [];
-  }
+  // can be optimized by replacing duplicates instead of re-generating whole array
+
+  let items: string[] = [];
+
+  do {
+    switch (gameMode) {
+      case "classic words":
+      case "flash words":
+        items = randomWords({ exactly: itemLength });
+        break;
+      case "classic numbers":
+      case "flash numbers":
+        items = randomNumberStrings();
+        break;
+      default:
+    }
+  } while (hasConsecutiveDuplicates(items));
+
+  return items;
 };
